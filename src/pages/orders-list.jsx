e01db524266616977514d906777128be5f6f0b1f
baucucu <alexandru.raduca@gmail.com>
2020-11-30 15:31:52 +0200

    no message

--- src/pages/orders-list.jsx
+++ src/pages/orders-list.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import {useState, useEffect} from 'react';
 import { Block, BlockTitle, useRef, Row, Col, Button,Card, Subnavbar, Searchbar, Page, Navbar, List, ListItem, ListGroup, Segmented } from 'framework7-react';
-import moment from 'moment';
 import _ from 'lodash';
 import { f7, f7ready } from 'framework7-react';
 
-var dateFormat = "YYYY/MM/DD";
-var timeFormat = "HH:mm";
-var region = 'Europe/Bucharest';
 
 export default function(props) {
   
@@ -15,6 +11,7 @@ export default function(props) {
   const [filters, setFilters] = useState(['AWAITING_PAYMENT','CANCELLED','AWAITING_PROCESSING','PROCESSING','SHIPPED','RETURNED'])
   
   useEffect(() => {  
+    console.log(process.env.TOKEN)
     fetch(`https://app.ecwid.com/api/v3/39042093/orders?token=${process.env.TOKEN}`,)
       .then(response => response.json())
       .then(data => {
@@ -63,31 +60,27 @@ export default function(props) {
         </Subnavbar>
         
       </Navbar>
-      <Card inset>
-        <List>
-          <ListItem
-            title='Filter orders'
-            smartSelect
-            smartSelectParams={{openIn: 'popup'}}
-            className='smart-select smart-select-init'
-          >
-            <select name='filter' multiple defaultValue={filters}>
-              <optgroup label='PAYMENT STATUS'>
-                <option value='PAID'>PAID</option>
-                <option value='AWAITING_PAYMENT'>AWAITING_PAYMENT</option>
-                <option value='CANCELLED'>CANCELLED</option>
-              </optgroup>
-              <optgroup label='FULFILLMENT STATUS'>
-                <option value='AWAITING_PROCESSING'>AWAITING_PROCESSING</option>
-                <option value='PROCESSING'>PROCESSING</option>
-                <option value='SHIPPED'>SHIPPED</option>
-                <option value='DELIVERED'>DELIVERED</option>
-                <option value='RETURNED'>RETURNED</option>
-              </optgroup>
-            </select>
-          </ListItem>
-        </List>
-      </Card>
+      <ListItem
+        title='Filter orders'
+        smartSelect
+        smartSelectParams={{openIn: 'popup'}}
+        className='smart-select smart-select-init'
+      >
+        <select name='filter' multiple defaultValue={filters}>
+          <optgroup label='PAYMENT STATUS'>
+            <option value='PAID'>PAID</option>
+            <option value='AWAITING_PAYMENT'>AWAITING_PAYMENT</option>
+            <option value='CANCELLED'>CANCELLED</option>
+          </optgroup>
+          <optgroup label='FULFILLMENT STATUS'>
+            <option value='AWAITING_PROCESSING'>AWAITING_PROCESSING</option>
+            <option value='PROCESSING'>PROCESSING</option>
+            <option value='SHIPPED'>SHIPPED</option>
+            <option value='DELIVERED'>DELIVERED</option>
+            <option value='RETURNED'>RETURNED</option>
+          </optgroup>
+        </select>
+      </ListItem>
       
       <Card>
         <BlockTitle>Orders: {filterOrders().length}</BlockTitle>
